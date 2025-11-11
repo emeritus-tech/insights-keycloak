4d97430310d6dad15f9b9854a89b8ba868554fce
ayushshrivastava027 <ayush.shrivastava@emeritus.org>
2025-11-11 15:57:43 +0530

    upgrade java version to 21

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 
 #curl https://download.oracle.com/java/20/archive/jdk-20_linux-x64_bin.deb --output java.deb
 
-curl https://download.java.net/java/GA/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz --output jdk.tar.gz
+curl https://download.oracle.com/java/21/archive/jdk-21_linux-x64_bin.tar.gz --output jdk.tar.gz
 
 #apt install -y libasound2 libc6-i386 libc6-x32 libfreetype6 libxi6 libxrender1 libxtst6
 
