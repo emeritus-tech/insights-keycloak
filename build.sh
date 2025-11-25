f9ad8dd45a323f71ae941d519d14d056842cfd43
ayushshrivastava027 <ayush.shrivastava@emeritus.org>
2025-11-25 16:51:59 +0530

    added slip proto check

--- build.sh
+++ build.sh
@@ -25,7 +25,8 @@ export PATH=$JAVA_HOME/bin:$PATH
 echo "--2-- $(which javac)"
 echo "--3-- $(readlink -f $( which javac ))"
 
-./mvnw -pl quarkus/deployment,quarkus/dist,themes, -am -DskipTests clean install | tee log-$(date +%H-%M-%y-%m-%d).txt
+# Skip proto lock check for development builds (network/proxy issues)
+./mvnw -pl quarkus/deployment,quarkus/dist,themes, -am -DskipTests -DskipProtoLock=true clean install | tee log-$(date +%H-%M-%y-%m-%d).txt
 
 echo "Running build command for MSQL database"
 
